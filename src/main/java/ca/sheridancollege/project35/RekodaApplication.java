86fa58faa8f90f42f02b06b2fb8e0c5f37ba47da
Han Nguyen <59750183+nguye340@users.noreply.github.com>
2024-01-21 02:55:54 -0500

    Add files via upload

---
+++ src/main/java/ca/sheridancollege/project35/RekodaApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.project35;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RekodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RekodaApplication.class, args);
+	}
+
+}
